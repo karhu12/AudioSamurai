ab8ad5473829bc0023f36a918a378daa5c426c92
Riku Kaipainen <shylessfly@gmail.com>
2019-11-05 17:14:52 +0200

    Created generic MapObject which represent enemy/obstacle on the map
    Created simple MapObject which represents an enemy or obstacle in the map. The MapObject responds to the players HitArea and Player colliders to trigger events. Each of the derived classes must implement methods they want to use and change Placement according to needs.

---
+++ AudioSamurai/Assets/Scripts/Game/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public const string COLLIDER_NAME = "Player";
+    public const string HIT_COLLIDER_NAME = "HitArea";
+}
