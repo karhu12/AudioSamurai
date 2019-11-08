04b592a46b85b851f30ce07aa4cbaefbe7309525
Toni Mettovaara <t6meto01@students.oamk.fi>
2019-11-08 15:58:11 +0200

    Added toggle function for running.

--- AudioSamurai/Assets/Scripts/Movement/PlayerMovement.cs
+++ AudioSamurai/Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,34 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float bpm;
     public Rigidbody rb;
     private float speed = 10f;
     private bool onGround = true;
-    private bool keyPressed = false;
+    private bool jumpKeyPressed = false;
+    private bool isRunning = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        bpm = bpm / 60f;
+        bpm /= 60f;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Constantly observe if the player has pressed the jump button.
-        checkIfJumpKeyPressed();
+        //Constantly observe if the player has pressed any buttons.
+        CheckPlayerInput();
     }
 
     void FixedUpdate()
     {
         //Add direction and velocity to player character depending on a song bpm
-        transform.position -= new Vector3(bpm * Time.deltaTime * 2, 0f, 0f);
-
+        if (isRunning)
+        {
+            transform.position -= new Vector3(bpm * Time.deltaTime * 2, 0f, 0f);
+        }
         //Check if character should jump 
-        if(onGround && keyPressed)
+        if (onGround && jumpKeyPressed)
         {
             float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
             float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
@@ -40,34 +42,38 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
             onGround = false;
         }
-        keyPressed = false;
-        
+        jumpKeyPressed = false;
+
         //Slow down the velocity a little bit when jumping in order to make the physics more realistic.
         //***THIS COULD SET THE PACE OFF BEAT SO MAY NEED TO LOOK INTO THIS IN THE FUTURE. NEEDS SOME TESTING. MIGHT BE IRRELEVANT.***
-        if(!onGround)
+        if (!onGround && isRunning)
         {
             transform.position += new Vector3(bpm * Time.deltaTime * 0.38f, 0f, 0f);
         }
     }
 
-    void checkIfJumpKeyPressed()
+    //Check what possible keys the player can press
+    void CheckPlayerInput()
     {
+        //Default jump key is Space, can be changed from Unity input settings
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log("space pressed");
-            keyPressed = true;
+            jumpKeyPressed = true;
+        }
+        //Default submit key is Enter, can be changed from Unity input settings
+        else if (Input.GetButtonDown("Submit"))
+        {
+            isRunning = !isRunning;
         }
     }
 
     //Check when character hits the ground to make the game logic reasonable. This way player can't jump while being mid-air.
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.tag == "Ground")
         {
             onGround = true;
         }
     }
-
-    
 }
+
