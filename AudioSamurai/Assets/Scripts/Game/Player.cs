81ead958403b9c1684527fe69359293d4ec1d523
Riku Kaipainen <shylessfly@gmail.com>
2019-11-08 13:14:24 +0200

    Created simple equipment system in order for the items to stick to bones
    Simple equipment system stitches equipment to the parents skeleton so when the armature is moved they will move with them.

--- AudioSamurai/Assets/Scripts/Game/Player.cs
+++ AudioSamurai/Assets/Scripts/Game/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
 public class Player : MonoBehaviour
 {
@@ -12,12 +13,18 @@ public class Player : MonoBehaviour
 
     public Collider hitCollider;
 
+    /* Cosmetic */
+    public GameObject hatModel;
+    public GameObject swordModel;
+
+    public Equipment Equipment { get; private set; }
+
     private IEnumerator jumpAttack;
     private IEnumerator attack;
 
     public const string COLLIDER_NAME = "Player";
     public const string HIT_COLLIDER_NAME = "HitArea";
-
+       
 
     public bool IsAttacking { get; private set; }
     public bool IsJumpAttacking { get; private set; }
@@ -27,6 +34,23 @@ public class Player : MonoBehaviour
         hitCollider.gameObject.SetActive(false);
         IsAttacking = false;
         IsJumpAttacking = false;
+
+        Equipment = new Equipment(gameObject);
+        StartCoroutine(EquipCoroutine()); 
+    }
+
+    /* Performs item equipping. NOTE : For some reason equipment is invisible after equipping if coroutine is not used to yield result after equip.*/
+    private IEnumerator EquipCoroutine()
+    {
+        if (hatModel != null)
+            Equipment.Equip(hatModel, "Hat");
+
+        yield return null;
+
+        if (swordModel != null)
+            Equipment.Equip(swordModel, "Katana");
+
+        yield return null;
     }
 
     private void Update()
@@ -41,6 +65,10 @@ public class Player : MonoBehaviour
         }
     }
 
+    /* 
+     * Performs an normal attack which is ran in an coroutine because it has time variables.
+     * Attack enables HitArea collider which is triggered on MapObjects if they collide.
+     */
     public void Attack()
     {
         attack = AttackCoroutine();
@@ -61,6 +89,10 @@ public class Player : MonoBehaviour
         hitCollider.gameObject.SetActive(false);
     }
 
+    /* 
+     * Performs an jump attack which is ran in an coroutine because it has time variables. 
+     * Works the same as normal attack but IsAttacking and IsJumpAttacking should be checked respectively.
+     */
     public void JumpAttack()
     {
         jumpAttack = JumpAttackCoroutine();
@@ -79,5 +111,52 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(ATTACK_TIME);
         IsJumpAttacking = false;
         hitCollider.gameObject.SetActive(false);
+    }
+}
+
+/* 
+ * Class that holds references to the game objects worn by the parent of equipment.
+ * Equipment works using Stitcher which stitches given equipments metarig to the metarig (bone armature) of parent object.
+ * You can equipt items to different slots given by a string.
+ */
+public class Equipment
+{
+    private GameObject parent;
+    private Dictionary<string, GameObject> equipment = new Dictionary<string, GameObject>();
+    private Stitcher stitcher = new Stitcher();
+
+    public Equipment(GameObject parent)
+    {
+        this.parent = parent;
+    }
+
+    public bool Equip(GameObject obj, string slot)
+    {
+        if (obj == null)
+            return false;
+
+        if (equipment.ContainsKey(slot))
+        {
+            GameObject worn = equipment[slot];
+            MonoBehaviour.Destroy(worn);
+            equipment.Remove(slot);
+        }
+
+        GameObject newEquipment = MonoBehaviour.Instantiate(obj);
+        stitcher.Stitch(newEquipment, parent);
+        equipment[slot] = newEquipment;
+        return true;
+    }
+
+    public bool Unequip(string slot)
+    {
+        if (equipment.ContainsKey(slot))
+        {
+            GameObject worn = equipment[slot];
+            MonoBehaviour.Destroy(worn);
+            equipment.Remove(slot);
+            return true;
+        }
+        return false;
     }
 }
