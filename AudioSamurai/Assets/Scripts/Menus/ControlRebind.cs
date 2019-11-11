e7e144ca36f7c450993d5f09310d7da4f2beba03
Topi <topi.frondelius@windowslive.com>
2019-11-11 13:14:01 +0200

    Finished options menu with working bindings

--- AudioSamurai/Assets/Scripts/Menus/ControlRebind.cs
+++ AudioSamurai/Assets/Scripts/Menus/ControlRebind.cs
@@ -3,28 +3,19 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 public class ControlRebind : MonoBehaviour
 {
-
     public Button button;
     public Text text;
     public InputActionReference actionReference;
     public int defaultBindingIndex;
-    public Button compositeButtons;
-    public Text[] compositeTexts;
+    public InputActionAsset inputActionAsset;
 
     private InputAction inputAction;
     private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
-    private bool isUsingComposite;
-
-    InputAction action;
-    public InputActionAsset inputActionAsset;
-
+    private bool isActive;
 
     void Start()
     {
-        var inputActions = inputActionAsset.FindActionMap("Player");
-        action = inputActions.FindAction("Attack");
-        inputActionAsset.Enable();
-        //inputAction = actionReference.action;
+        inputAction = actionReference.action;
         if (button == null)
         {
             button = GetComponentInChildren<Button>();
@@ -36,6 +27,7 @@ public class ControlRebind : MonoBehaviour
         }
 
         button.onClick.AddListener(delegate { RemapButtonClicked(name, defaultBindingIndex); });
+        ResetButtonMappingTextValue();
         
     }
 
@@ -46,43 +38,30 @@ public class ControlRebind : MonoBehaviour
 
     void RemapButtonClicked(string name, int bindingIndex = 0)
     {
-        //InputAction actionToUse = inputAction.actionMap[0].actions[0];
-        //inputAction.Disable();
-        //actionToUse.Disable();
-        /*button.enabled = false;
-        text.text = "Press button";
+        inputAction.Disable();
+        button.enabled = false;
+        text.text = "Press any key...";
+
         rebindingOperation?.Dispose();
         rebindingOperation = inputAction.PerformInteractiveRebinding()
             .WithControlsExcluding("<Mouse>/position")
             .WithControlsExcluding("<Mouse>/delta")
             .OnMatchWaitForAnother(0.1f)
             .OnComplete(operation => ButtonRebindCompleted());
-        rebindingOperation.Start();*/
-
-        var inputActions = inputActionAsset.FindActionMap("Player");
-        action = inputActions.FindAction("Attack");
-        inputActionAsset.Disable();
-        action.Disable();
-        rebindingOperation = action.PerformInteractiveRebinding().WithRebindAddingNewBinding()
-            .OnComplete(operation =>
-            {
-                action.Enable();
-                inputActionAsset.Enable();
-                ButtonRebindCompleted(GetComponent<Button>());
-            })
-            .Start();
+        rebindingOperation.Start();
     }
 
     void ResetButtonMappingTextValue()
     {
         text.text = InputControlPath.ToHumanReadableString(inputAction.bindings[0].effectivePath);
-        button.gameObject.SetActive(!isUsingComposite);
+        button.gameObject.SetActive(!isActive);
     }
 
-    void ButtonRebindCompleted(Button button)
+    void ButtonRebindCompleted()
     {
+        inputAction.Enable();
         rebindingOperation.Dispose();
-        //rebindingOperation = null;
+        rebindingOperation = null;
         ResetButtonMappingTextValue();
         button.enabled = true;
     }
