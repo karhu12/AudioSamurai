ff6112c4348b7faa6d6b802d9a1d0ad0aa758b54
Juho Parkkinen <parkkisenjuho@gmail.com>
2019-11-11 16:48:13 +0200

    PauseMenu added
    Added PauseMenu with working buttons

---
+++ AudioSamurai/Assets/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool onPause = false;
+
+    public GameObject pauseUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (onPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    //ResumeButton OnClick
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        onPause = false;
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        onPause = true;
+    }
+
+    //MenuButton OnClick
+    public void LoadMainMenu()
+    {
+        pauseUI.SetActive(false);
+        CameraController.Instance.SetCameraToState(CameraController.CameraState.Menu);
+        Debug.Log("Loading main menu...");
+    }
+}
