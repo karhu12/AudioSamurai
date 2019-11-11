cffb75c67f42217664d14451692365afd85a239e
Toni Mettovaara <t6meto01@students.oamk.fi>
2019-11-11 15:21:50 +0200

    Added basic combo and score system with base class and placeholder values. Both values are being accessed through MapObject script.

---
+++ AudioSamurai/Assets/Scripts/Game/ScoreSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public GameObject comboText;
+
+    public const int MIN_COMBO = 1;
+    public const int MAX_COMBO = 16;
+
+    public static int combo;
+    public static int score;
+
+    private void Start()
+    {
+        combo = MIN_COMBO;
+        score = 0;
+    }
+
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "SCORE: " + score;
+        comboText.GetComponent<Text>().text = "COMBO: " + combo + "x";
+        AddScore();
+    }
+
+    //Add basic score from proggressing. Combo, hits and misses are handled in MapObject script.
+    void AddScore()
+    {
+        score += 1 * combo;
+    }
+}
