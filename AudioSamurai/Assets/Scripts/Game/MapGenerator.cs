612714a63e0024588f672797cab35881606a739d
Riku Kaipainen <shylessfly@gmail.com>
2019-11-15 11:20:35 +0200

    Finished improved map generator.

--- AudioSamurai/Assets/Scripts/Game/MapGenerator.cs
+++ AudioSamurai/Assets/Scripts/Game/MapGenerator.cs
@@ -1,25 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class MapGenerator : MonoBehaviour
 {
     /* Constants */
     public const float SCALED_SIZE = 800;
     public const float PLACEMENT_OFFSET = 16;
-    public const int SPAWN_AHEAD_OF_TARGET = 3;
+    public const int SPAWN_AFTER_TARGET = 3;
+    public const int SPAWN_BEFORE_TARGET = 1;
 
     public GameObject generationTarget;
     public GameObject generationParent;
     public List<Generatable> tilePrefabs;
     public Dictionary<string, ObjectPool> tileObjectPools = new Dictionary<string, ObjectPool>();
-    public Quaternion defaultOrientation;
-    public Direction generationDirection;
 
     private List<Generatable> spawnedTiles = new List<Generatable>();
     private string currentGenerationType;
-
-    public enum Direction { X, Y, Z }
 
     public enum GenerationState
     {
@@ -59,103 +57,30 @@ public class MapGenerator : MonoBehaviour
 
     private void Update()
     {
-        if (State == GenerationState.Generating)
-        {
-            if (NeedsSpawn())
-            {
-                Generatable obj = spawnedTiles.FindLast((tile) => GetTargetDirectionPos(tile) > GetTargetDirectionPos(generationTarget));
-
-                float targetPos = GetTargetDirectionPos(generationTarget);
-                if (obj == null)
-                {
-                    for (int i = 0; i < SPAWN_AHEAD_OF_TARGET; i++)
-                    {
-                        Generatable tile = (Generatable)tileObjectPools[currentGenerationType].Get();
-                        SetTargetDirectionPos(tile.gameObject, targetPos + i * SPAWN_AHEAD_OF_TARGET);
-                        spawnedTiles.Add(tile);
-                    }
-                }
-                else
-                {
-                    Generatable newTile = (Generatable)tileObjectPools[currentGenerationType].Get();
-                    SetTargetDirectionPos(newTile.gameObject, GetTargetDirectionPos(obj.gameObject) + PLACEMENT_OFFSET);
-                    spawnedTiles.Add(newTile);
-                }
-            }
-
+        if (State == GenerationState.Generating) {
             List<Generatable> removeList = new List<Generatable>();
-            foreach (var tile in spawnedTiles)
-            {
-                if (GetTargetDirectionPos(tile) - GetTargetDirectionPos(generationTarget) <= -PLACEMENT_OFFSET)
-                {
-                    tile.ReturnToPool();
-                    removeList.Add(tile);
-                }
+            foreach (var tile in spawnedTiles) {
+                float diff = tile.transform.position.z - generationTarget.transform.position.z;
+                if (diff < 0 && diff > -PLACEMENT_OFFSET * (SPAWN_BEFORE_TARGET + 1))
+                    continue;
+                else if (diff >= 0 && diff < PLACEMENT_OFFSET * (SPAWN_AFTER_TARGET + 1))
+                    continue;
+                tile.ReturnToPool();
+                removeList.Add(tile);
             }
             foreach (var remove in removeList) { spawnedTiles.Remove(remove); }
-        }
-    }
 
-    private bool NeedsSpawn()
-    {
-        bool needsNewSpawn = true;
-        foreach (var spawned in spawnedTiles)
-        {
-            float offset = GetTargetDirectionPos(spawned) - GetTargetDirectionPos(generationTarget);
-
-            if (offset >= SPAWN_AHEAD_OF_TARGET * PLACEMENT_OFFSET)
-                needsNewSpawn = false;
-
-            if (!needsNewSpawn)
-                break;
-        }
-        return needsNewSpawn;
-    }
-
-    private void Orientate(GameObject tile)
-    {
-        Vector3 objectSize = Vector3.Scale(tile.transform.localScale, tile.gameObject.GetComponent<Mesh>().bounds.size);
-        tile.transform.localScale.Set(
-            (objectSize.x < SCALED_SIZE) ? (objectSize.x / SCALED_SIZE) * objectSize.x : 0,
-            (objectSize.y < SCALED_SIZE) ? (objectSize.y / SCALED_SIZE) * objectSize.y : 0,
-            (objectSize.z < SCALED_SIZE) ? (objectSize.z / SCALED_SIZE) * objectSize.z : 0
-        );
-        tile.transform.SetPositionAndRotation(new Vector3(0, 0, 0), defaultOrientation);
-    }
-
-    private float GetTargetDirectionPos(GameObject target)
-    {
-        switch (generationDirection)
-        {
-            case Direction.X:
-                return target.transform.position.x;
-            case Direction.Y:
-                return target.transform.position.y;
-            case Direction.Z:
-                return target.transform.position.z;
-        }
-        generationDirection = Direction.Z;
-        return target.transform.position.z;
-    }
-
-    private float GetTargetDirectionPos(Generatable target)
-    {
-        return GetTargetDirectionPos(target.gameObject);
-    }
-
-    private void SetTargetDirectionPos(GameObject target, float pos)
-    {
-        switch (generationDirection)
-        {
-            case Direction.X:
-                target.transform.position = new Vector3(pos, 0, 0);
-                break;
-            case Direction.Y:
-                target.transform.position = new Vector3(0, pos, 0);
-                break;
-            case Direction.Z:
-                target.transform.position = new Vector3(0, 0, pos);
-                break;
+            for (int tilePos = -SPAWN_BEFORE_TARGET; tilePos < SPAWN_AFTER_TARGET; tilePos++) {
+                float center = generationTarget.transform.position.z + (tilePos * PLACEMENT_OFFSET);
+                float backPosition = center - PLACEMENT_OFFSET;
+                float forwardPosition = center + PLACEMENT_OFFSET;
+                if (!spawnedTiles.Exists(item => item.transform.position.z > backPosition && item.transform.position.z <= forwardPosition)) 
+                {
+                    Generatable g = (Generatable)tileObjectPools[currentGenerationType].Get();
+                    g.transform.position = new Vector3(0, 0, (int)center);
+                    spawnedTiles.Add(g);
+                }
+            }
         }
     }
 }
