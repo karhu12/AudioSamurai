9a2c38f8f0d05858e3f6f9b7004a6e8252d13a96
Riku Kaipainen <shylessfly@gmail.com>
2019-11-19 15:55:39 +0200

    Implemented simple fail screen with fail music and ability to restart

---
+++ AudioSamurai/Assets/Scripts/Menus/FailMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailMenu : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip failMusic;
+
+    /* Constants */
+    public const string TAG = "FailMenu";
+
+    public void ToggleFailMusic(bool on) {
+        if (on) {
+            audioSource.PlayOneShot(failMusic);
+        } else {
+            audioSource.Stop();
+        }
+    }
+
+    public void OnRetry() {
+        GameController.Instance.Retry();
+        ToggleFailMusic(false);
+    }
+
+    public void OnBackToMenu() {
+        GameController.Instance.QuitGame();
+        ToggleFailMusic(false);
+    }
+}
