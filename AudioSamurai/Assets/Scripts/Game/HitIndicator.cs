dee54a20a268970d883c7672e0e5a1442e2a7119
Riku Kaipainen <shylessfly@gmail.com>
2019-11-25 10:02:30 +0200

    Created hit indicator assets with pop animation

---
+++ AudioSamurai/Assets/Scripts/Game/HitIndicator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitIndicator : MonoBehaviour
+{
+    public const string POP_TRIGGER = "Pop";
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
+    public void Pop()
+    {
+        animator.SetTrigger(POP_TRIGGER);
+    }
+}
