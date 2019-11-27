b9296b88a2f49df41c5cee88b84fe1043cfc096a
Olli J <olli.jarvela@hotmail.com>
2019-11-27 11:42:05 +0200

    Mode menu done

--- AudioSamurai/Assets/Scripts/Menus/ModMenu.cs
+++ AudioSamurai/Assets/Scripts/Menus/ModMenu.cs
@@ -6,36 +6,28 @@ using UnityEngine.Events;
 
 public class ModMenu : Singleton<ModMenu>
 {
-
-    public Toggle normalModeToggle;
     public Toggle sdModeToggle;
     public Toggle nfModeToggle;
-
-    private int normalMode;
+    public GameObject Panel;
+   
     private int sdMode;
     private int nfMode;
 
-    private const string NORLMA_MODE_PREF = "NormalMode";
     private const string SD_MODE_PREF = "SuddenDeathMode";
     private const string NF_MODE_PREF = "NoFailMode";
 
-    void Awake()
+    private new void Awake()
     {
-
-        normalMode = PlayerPrefs.GetInt(NORLMA_MODE_PREF);
         sdMode = PlayerPrefs.GetInt(SD_MODE_PREF);
         nfMode = PlayerPrefs.GetInt(NF_MODE_PREF);
 
         if (sdMode == 1) {
-            normalModeToggle.isOn = false;
             sdModeToggle.isOn = true;
             nfModeToggle.isOn = false;
         }else if (nfMode == 1) {
-            normalModeToggle.isOn = false;
             sdModeToggle.isOn = false;
             nfModeToggle.isOn = true;
         }else{
-            normalModeToggle.isOn = true;
             sdModeToggle.isOn = false;
             nfModeToggle.isOn = false;
         }
@@ -43,31 +35,25 @@ public class ModMenu : Singleton<ModMenu>
 
     public void ToggleView()
     {
-
+        if (Panel != null)
+        {
+            Animator animator = Panel.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("open");
+                animator.SetBool("open", !isOpen);
+            }
+        }
     }
 
     public int ReturnMode()
     {
         if (sdMode == 1)
             return 100;
-
         else if (nfMode == 1)
             return 0;
    
         else return 1;
-    }
-
-    public void NormalModeToggle(bool modeState)
-    {
-        if (modeState == false)
-        {
-            PlayerPrefs.SetInt(NORLMA_MODE_PREF, 0);
-        }
-
-        else
-        {
-            PlayerPrefs.SetInt(NORLMA_MODE_PREF, 1);
-        }
     }
 
     public void SDModeToggle(bool modeState)
