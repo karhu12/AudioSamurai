0feb0227f79d07d6e73dc7b1a2fab42642d89d58
Juho Parkkinen <parkkisenjuho@gmail.com>
2019-12-02 13:11:18 +0200

    Added ShowHideToggle script
    Toggling between different help buttons works properly now

---
+++ AudioSamurai/Assets/Scripts/Menus/ShowHideToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideToggle : MonoBehaviour
+{
+
+    public List<GameObject> hideList;
+    public List<GameObject> showList;
+
+    public void Toggle(bool show)
+    {
+        foreach(var obj in hideList)
+        {
+            if (show)
+                obj.SetActive(false);
+            else
+                obj.SetActive(true);
+        }
+        foreach (var obj in showList)
+        {
+            if (show)
+                obj.SetActive(true);
+            else
+                obj.SetActive(false);
+        }
+    }
+}
